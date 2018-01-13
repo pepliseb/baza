1f97d187e43d690e28db69e9c301a09bfddd8690
pepliseb <eman@onet.eu>
2018-01-13 12:56:22 +0100

    add controller

--- src/main/java/pl/pepliseb/baza/controllers/MainController.java
+++ src/main/java/pl/pepliseb/baza/controllers/MainController.java
@@ -3,6 +3,7 @@ package pl.pepliseb.baza.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.pepliseb.baza.models.services.StringService;
 
 
 @Controller
@@ -10,7 +11,8 @@ public class MainController {
 
     @GetMapping("/")
     @ResponseBody
-    public String index() {return "Czesc!";
+    public String index() {
+        return "Czesc!" + new StringService().generateRandomString();
 
 
     }

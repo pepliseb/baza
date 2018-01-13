7f4b286530466ca309f27a52afa79a4109256aed
pepliseb <eman@onet.eu>
2018-01-13 12:52:19 +0100

    add controller

--- src/main/java/pl/pepliseb/baza/models/services/StringService.java
+++ src/main/java/pl/pepliseb/baza/models/services/StringService.java
@@ -1,4 +1,10 @@
 package pl.pepliseb.baza.models.services;
 
+import java.util.UUID;
+
 public class StringService {
+
+    public String generateRandomString(){
+        return UUID.randomUUID().toString();
+    }
 }

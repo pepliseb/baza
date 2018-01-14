f9e42531ba980858bac5b899eb464ce62d5a2fc3
pepliseb <eman@onet.eu>
2018-01-14 17:15:20 +0100

    add tabela reservation

--- src/main/java/pl/pepliseb/baza/models/ReservationModel.java
+++ src/main/java/pl/pepliseb/baza/models/ReservationModel.java
@@ -2,10 +2,15 @@ package pl.pepliseb.baza.models;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.sql.Date;
 
 @Data
+@Entity
+@Table(name = "reservation")
 public class ReservationModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String lastname;

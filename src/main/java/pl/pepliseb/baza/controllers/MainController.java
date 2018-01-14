f30bf7747f16f097261bea1de9fd444c1f740031
pepliseb <eman@onet.eu>
2018-01-14 21:46:58 +0100

    add tabela reservation

--- src/main/java/pl/pepliseb/baza/controllers/MainController.java
+++ src/main/java/pl/pepliseb/baza/controllers/MainController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.pepliseb.baza.models.ReservationModel;
 import pl.pepliseb.baza.models.repositories.ReservationRepository;
 import pl.pepliseb.baza.models.services.StringService;
+
+import java.sql.Date;
+import java.util.Calendar;
 
 
 @Controller
@@ -21,11 +25,15 @@ public class MainController {
     @GetMapping("/")
     @ResponseBody
     public String index() {
-//        w tym miejscu zapisujemy recznie do tabeli (encji)
+//        w tym miejscu zapisujemy recznie cos do tabeli (encji)
+        ReservationModel model = new ReservationModel();
+        model.setAdres("Opole");
+        model.setDate(new Date(Calendar.getInstance().getTimeInMillis()));
+        model.setLastname("Sebastian");
+        model.setName("Nowak");
+        reservationRepository.save(model);
                 return "Hej, zapisałem dane ";
 
-
     }
-
 
 }

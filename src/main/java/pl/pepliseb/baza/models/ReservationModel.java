7f6927ff18c603f971c8e59152364948b518cba7
pepliseb <eman@onet.eu>
2018-01-14 17:09:08 +0100

    add tabela reservation

--- src/main/java/pl/pepliseb/baza/models/ReservationModel.java
+++ src/main/java/pl/pepliseb/baza/models/ReservationModel.java
@@ -1,5 +1,15 @@
 package pl.pepliseb.baza.models;
 
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
 public class ReservationModel {
+    private int id;
+    private String name;
+    private String lastname;
+    private Date date;
+    private String adres;
 
 }
